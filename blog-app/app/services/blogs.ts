c6d58b43eace8af3a05f0906f495b65e68c8bacb
Prince_ij <aminuibrahimthefifth@gmail.com>
2026-05-21 17:18:40 +0100

    fix ts

--- blog-app/app/services/blogs.ts
+++ blog-app/app/services/blogs.ts
@@ -7,11 +7,11 @@ export const getBlogs = async () => {
 };
 
 export const addBlog = async (title: string, author: string, url: string) => {
-  const user = await db.query.users.findFirst({
-    orderBy: sql`RANDOM()`,
-  });
-  
-  await db.insert(blogs).values({ title, author, url, userId: user.id });
+  // const user = await db.query.users.findFirst({
+  //   orderBy: sql`RANDOM()`,
+  // });
+
+  await db.insert(blogs).values({ title, author, url, userId: 2 });
 };
 
 export const getBlogById = async (id: number) => {
