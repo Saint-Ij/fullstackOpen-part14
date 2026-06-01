94419967648fb0e7c35340b4d6084e1e4b60d3c8
Prince_ij <aminuibrahimthefifth@gmail.com>
2026-06-01 19:35:16 +0100

    implement useActionState error

--- blog-app/app/actions/blogs.ts
+++ blog-app/app/actions/blogs.ts
@@ -6,25 +6,48 @@ import { redirect } from "next/navigation";
 import { addBlog } from "../services/blogs";
 import { addLikes } from "../services/blogs";
 
+type FormError = {
+  title?: string;
+  author?: string;
+  url?: string;
+};
+export type FormState = {
+  errors: FormError,
+  values: {title: string, url: string, author: string}
+}
+
 export const createNewBlog = async (
-  prevState: { error: string },
+  prevState: {
+    errors: FormError;
+    values: { author: string; title: string; url: string };
+  },
   formData: FormData
-) => {
+): Promise<FormState> => {
   const session = await auth();
   if (!session) {
     redirect("/login");
   }
+
   const title = formData.get("title") as string;
-  if (!title || title.length < 5) {
-    return { error: "title required and must be 5 chars long" };
-  }
   const author = formData.get("author") as string;
-  if (!author || author.length < 5) {
-    return { error: "author required and must be 5 chars long" };
-  }
   const url = formData.get("url") as string;
+
+  const errors: Record<string, string> = {};
+
+  if (!title || title.length < 5) {
+    errors.title = "title required and must be 5 chars long";
+  }
+
+  if (!author || author.length < 5) {
+    errors.author = "author required and must be 5 chars long";
+  }
+
   if (!url || url.length < 5) {
-    return { error: "url required and must be 5 chars long" };
+    errors.url = "url required and must be 5 chars long";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors, values: { title, author, url } };
   }
 
   await addBlog(title, author, url);
--- blog-app/app/blogs/new/page.tsx
+++ blog-app/app/blogs/new/page.tsx
@@ -1,25 +1,41 @@
 "use client";
 
-import { createNewBlog } from "@/app/actions/blogs";
+import { createNewBlog, FormState} from "@/app/actions/blogs";
 import { useActionState } from "react";
 
 const NewBlog = () => {
-  const [state, formAction] = useActionState(createNewBlog, { error: "" });
+  const initialState:  FormState  = {
+    errors: {},
+    values: {
+      title: "",
+      author: "",
+      url: "",
+    },
+  };
+
+
+  const [state, formAction] = useActionState(createNewBlog, initialState);
   return (
     <form style={formStyles} action={formAction}>
       <label>
-        Title: <input type="text" name="title" />
+        Title: <input type="text" name="title" defaultValue={state.values.title} />
       </label>
+      {state.errors.title && (
+        <p style={{ color: "red" }}>{state.errors.title}</p>
+      )}
       <label>
-        Author: <input type="text" name="author" />
+        Author: <input type="text" name="author" defaultValue={state.values.author}/>
       </label>
+      {state.errors.author && (
+        <p style={{ color: "red" }}>{state.errors.author}</p>
+      )}
       <label>
-        Url: <input type="text" name="url" />
+        Url: <input type="text" name="url" defaultValue={state.values.url}/>
       </label>
+      {state.errors.url && <p style={{color: "red"}}>{state.errors.url}</p>}
       <button type="submit" style={buttonStyle}>
         Submit
       </button>
-      {state.error && <p style={{ color: "red" }}>{state.error}</p>}
     </form>
   );
 };
