4ee14b8a52a77f730384138836acf2b692a0259e
Prince_ij <aminuibrahimthefifth@gmail.com>
2026-06-01 21:55:15 +0100

    implement register validation

--- blog-app/app/actions/users.ts
+++ blog-app/app/actions/users.ts
@@ -2,13 +2,56 @@
 
 import { redirect } from "next/navigation";
 import bcrypt from "bcryptjs";
+import { eq } from "drizzle-orm";
 import { db } from "../db";
 import { users } from "../db/schema";
 
-export const registerUser = async (formData: FormData) => {
+type userFormError = {
+  username?: string;
+  password?: string;
+};
+
+export type userFormState = {
+  errors: userFormError;
+  values: { username: string; password: string; cpassword: string, name: string };
+};
+
+export const registerUser = async (
+  prevState: {
+    errors: userFormError;
+    values: { username: string; password: string; cpassword: string };
+  },
+  formData: FormData
+): Promise<userFormState> => {
+  const errors: userFormError = {};
+
   const username = (formData.get("username") as string)?.trim();
   const name = (formData.get("name") as string)?.trim();
   const password = formData.get("password") as string;
+  const cpassword = formData.get("cpassword") as string;
+
+  const user = await db.query.users.findFirst({
+    where: eq(users.username, username),
+  });
+  if (user) {
+    errors.username = "username already exists";
+  }
+
+  if (cpassword !== password) {
+    errors.password = "passwords must match";
+  }
+
+  if (username.length < 4) {
+    errors["username"] = "username must be atleast 4 chars long";
+  }
+
+  if (password.length < 4) {
+    errors.password = "password must be atleast 4 chars long";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors, values: { username, password, cpassword, name } };
+  }
 
   const passwordHash = await bcrypt.hash(password, 10);
 
--- blog-app/app/register/page.tsx
+++ blog-app/app/register/page.tsx
@@ -1,27 +1,72 @@
-import { registerUser } from "../actions/users";
+"use client";
+
+import { registerUser, userFormState } from "../actions/users";
+import { useActionState } from "react";
 
 export default function RegisterPage() {
+  const initialState: userFormState = {
+    errors: {},
+    values: {
+      username: "",
+      password: "",
+      cpassword: "",
+      name: ""
+    },
+  };
+  const [state, formAction] = useActionState(registerUser, initialState);
   return (
     <div>
       <h2>Register</h2>
-      <form action={registerUser}>
+      <form action={formAction}>
         <div>
           <label>
             Username
-            <input type="text" name="username" required />
+            <input
+              type="text"
+              name="username"
+              required
+              defaultValue={state.values.username}
+            />
           </label>
+          {state.errors.username && (
+            <p style={{ color: "red" }}>{state.errors.username}</p>
+          )}
         </div>
         <div>
           <label>
             Name
-            <input type="text" name="name" required />
+            <input
+              type="text"
+              name="name"
+              required
+              defaultValue={state.values.name}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input type="password" name="password" required />
+            <input
+              type="password"
+              name="password"
+              required
+              defaultValue={state.values.password}
+            />
           </label>
+        </div>
+        <div>
+          <label>
+            Password
+            <input
+              type="password"
+              name="cpassword"
+              required
+              defaultValue={state.values.cpassword}
+            />
+          </label>
+          {state.errors.password && (
+            <p style={{ color: "red" }}>{state.errors.password}</p>
+          )}
         </div>
         <button type="submit">Register</button>
       </form>
