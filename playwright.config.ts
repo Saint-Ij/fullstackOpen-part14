045f38eed33ca2c82bf3e2e1ca98b65901753e64
Prince_ij <aminuibrahimthefifth@gmail.com>
2026-06-03 10:16:40 +0100

    fix config

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   },
 
   webServer: {
-    command: "npm run dev",
+    command: "npm run build && npm run start",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
